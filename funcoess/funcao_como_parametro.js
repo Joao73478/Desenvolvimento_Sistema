fade630beec114f2cbebbf11b269a1960d7982a6
joao73478 <fernandesjoaovitor411@gmail.com>
2025-05-29 20:15:18 -0300

    tipos de functions.js

---
+++ funcoess/funcao_como_parametro.js
@@ -0,0 +1,12 @@
+function executarOperacao(a, b, operacao) {
+    return operacao(a, b);
+
+}
+
+function multiplicar(x, y) {
+    return x * y;
+}
+
+let resultado = executarOperacao(4, 5, multiplicar);
+
+console.log("Multiplicacao: " + resultado);
